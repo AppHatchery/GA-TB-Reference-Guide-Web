0bd94875092a23a348308671bbcc8a5e0c481f52
MaxwellKJr <maxwellkjr@gmail.com>
2025-05-21 15:42:00 +0200

    Updated tables to new design with tabs

--- assets/main.js
+++ assets/main.js
@@ -9,19 +9,64 @@ function switchTab(tabIndex) {
     
   document.querySelectorAll('.tab')[tabIndex].classList.add('active-tab');
   document.getElementById(`tab-content${tabIndex}`).classList.add('active-tab');
+  console.log("TAB INDEX", tabIndex)
 }
 
 // This is for special cases like Table 7 and Table 14
 function switchOption(optionIndex) {
-    document.querySelectorAll('.tab').forEach(tab => {
-        tab.classList.remove('active-tab');
+
+    document.querySelectorAll('.tab-button').forEach(tab => {
+        tab.classList.remove('active-option');
     });
     
     document.querySelectorAll('.option-content').forEach(content => {
         content.classList.remove('active-option');
     });
 
-    document.querySelectorAll('.tab')[optionIndex].classList.add('active-tab');
+    document.querySelectorAll('.tab-button')[optionIndex].classList.add('active-option');
     document.querySelectorAll('.option-content')[optionIndex].classList.add('active-option');
-    document.getElementById(`content${optionIndex}`).classList.add('active-option');
+    document.getElementById(`option-content${optionIndex}`).classList.add('active-option');
+    console.log("OPTION INDEX", optionIndex)
+}
+
+function switchTable1Tab(tabIndex) {
+  document.querySelectorAll('.tab').forEach(tab => {
+      tab.classList.remove('active-tab');
+  });
+  
+  document.querySelectorAll('.tab-content').forEach(content => {
+      content.classList.remove('active-tab');
+  });
+    
+  document.querySelectorAll('.tab')[tabIndex].classList.add('active-tab');
+  document.getElementById(`tab-content${tabIndex}`).classList.add('active-tab');
+  console.log("TAB INDEX", tabIndex)
+}
+
+function switchTable2Tab(tabIndex) {
+    document.querySelectorAll('.tab2').forEach(tab => {
+        tab.classList.remove('active-tab');
+    })
+    
+    document.querySelectorAll('.tab2-content').forEach(content => {
+        content.classList.remove('active-tab');
+    });
+
+    document.querySelectorAll('.tab2')[tabIndex].classList.add('active-tab');
+    document.querySelectorAll('.tab2-content')[tabIndex].classList.add('active-tab');
+    console.log("TAB INDEX", tabIndex)
+}
+
+function switchTable3Tab(tabIndex) {
+  document.querySelectorAll('.tab3').forEach(tab => {
+      tab.classList.remove('active-tab');
+  })
+  
+  document.querySelectorAll('.tab3-content').forEach(content => {
+      content.classList.remove('active-tab');
+  });
+
+  document.querySelectorAll('.tab3')[tabIndex].classList.add('active-tab');
+  document.querySelectorAll('.tab3-content')[tabIndex].classList.add('active-tab');
+  console.log("TAB INDEX", tabIndex)
 }
